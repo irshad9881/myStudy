6e81d213248d67577feedbd004d7148d8ac6f731
Irshad <moirshad588@gmail.com>
2025-06-04 12:02:11 +0530

    user authentication & password reset routes related comments

---
+++ backend/routes/user.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+
+// Controllers
+const {
+    signup,
+    login,
+    sendOTP,
+    changePassword
+} = require('../controllers/auth');
+
+// Resetpassword controllers
+const {
+    resetPasswordToken,
+    resetPassword,
+} = require('../controllers/resetPassword');
+
+
+// Middleware
+const { auth } = require('../middleware/auth');
+
+// **************************************************
+//             Authentication routes
+// **************************************************
+
+// Route for user signup
+router.post('/signup', signup);
+
+// Route for user login
+router.post('/login', login);
+
+// Route for sending OTP to the user's email
+router.post('/sendotp', sendOTP);
+
+// Route for Changing the password
+router.post('/changepassword', auth, changePassword);
+
+
+
+// ******************************************* 
+//              Reset Password
+// ******************************************* 
+
+// Route for generating a reset password token
+router.post('/reset-password-token', resetPasswordToken);
+
+// Route for resetting user's password after verification
+router.post("/reset-password", resetPassword)
+
+
+module.exports = router;
