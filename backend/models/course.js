a3109b35f304a278a573e0b43ea768a10bc3e4cd
Irshad <moirshad588@gmail.com>
2025-06-04 17:12:08 +0530

    define model  for course

---
+++ backend/models/course.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema(
+  {
+    courseName: {
+      type: String,
+    },
+    courseDescription: {
+      type: String,
+    },
+    instructor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    whatYouWillLearn: {
+      type: String,
+    },
+    courseContent: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Section",
+      },
+    ],
+    ratingAndReviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "RatingAndReview",
+      },
+    ],
+    price: {
+      type: Number,
+    },
+    thumbnail: {
+      type: String,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    tag: {
+      type: [String],
+      required: true,
+    },
+    // studentsEnrolled: [
+    //   {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "User",
+    //     required: true,
+    //   },
+    // ]
+    //
+    studentsEnrolled: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      default: [],
+    },
+
+    instructions: {
+      type: [String],
+    },
+    status: {
+      type: String,
+      enum: ["Draft", "Published"],
+      default: "Draft",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Course", courseSchema);
